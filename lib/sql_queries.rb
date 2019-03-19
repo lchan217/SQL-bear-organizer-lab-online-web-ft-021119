0be1b7ca0e20500badc146a2596ab71afbe2bb6c
Lizzie Chan <lchan217@gmail.com>
2019-03-19 15:48:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,6 +27,6 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT name FROM bears WHERE id = 8"
+  "SELECT * FROM bears WHERE id = 8"
  # "SELECT name FROM bears WHERE name IS NULL, age = 20, gender = 'M', color = 'black', temperament = 'aggressive', alive = 0"
 end
